053c1d1f7fd37e3ce88a2b9a80feae4e76c5b683
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-05-25 09:51:07 +0700

    Week02_c: OOP basics, Getter and Setter in C# so different in Java. In C#, Boiler Place

--- Session03-OOP/SundayShop/Giaolang.Shop/Entities/Fruit.cs
+++ Session03-OOP/SundayShop/Giaolang.Shop/Entities/Fruit.cs
@@ -13,19 +13,51 @@ namespace Giaolang.Shop.Entities
 
         private string _description;        //_desc : __________________________
 
-        private double _price;              //_price: _____.__
+        private double _price;              //_price: _____.__ //thập phân
+
+        //hàm Get() trả về info lẻ, ToString() trả full, trả sỉ
+        //y chang: tên bạn là gì, học ngành gì, sinh năm bao nhiêu?
+
+        public string GetName()
+        {
+            return _name; //ko cần this. nếu thích cứ làm, ko sai!!!
+        }
+        public string GetDesc() => _description; //ko cần this. nếu thích cứ làm, ko sai!!!
+        //cú pháp rút gọn hàm chỉ có 1 câu lệnh: expression bodied
+
+        public double GetPrice() => _price;
+
+        //HÀM SET, NHẬN ĐẦU VÀO ĐÈ VÀO BÊN TRONG TƯƠNG ỨNG NHƯ BÊN JAVA
+        //TẤY, XÓA, SỬA BÀI LÀM TRƯỚC KHI NỘP, SETTING CÁI CẤU HÌNH ĐIỆN THOẠI MỚI MUA
+        public void SetName(string name)
+        {
+            this._name = name; // ko cần do đã khác tên biến
+        }
+
+        public void SetDesc(string desc) => _description = desc; // ko cần do đã khác tên biến
+
+        public void SetPrice(double price) => _price = price;
+
+        public void SetId(string id) => _id = id;
 
         public Fruit()
         {
         }
 
+        /// <summary>
+        /// Tạo mới thông tin trái cây
+        /// </summary>
+        /// <param name="id">mã số trái cây max 5 ký tự</param>
+        /// <param name="name">tên trái cây max 50</param>
+        /// <param name="description">mô tả trái cây max 255</param>
+        /// <param name="price">giá tiền số thực .</param>
         public Fruit(string id, string name, string description, double price)
         {
-            _id = id;
+            _id = id; //ko gây xung đột tên
             this._name = name;  //thừa this. dù luôn có this
                                 //con trỏ trỏ chính mình, cái tui cá nhân
                                 //dùng nó khi có sự nhầm lẫn giữa biến bên ngoài / tham số hàm và biến nội tại của object, class
-                                //members of a class (_id, _name, _desc, _price
+                                //members of a class (_id, _name, _desc, _price)
             _description = description;
             _price = price;
         }
@@ -47,7 +79,7 @@ namespace Giaolang.Shop.Entities
         //toString(): xem hết, get hết info trong 1 lượt
         //C#: Get, Set, ToString() 
 
-
+        //C# đưa ra 1 cơ chế, get, set đặt biệt
 
     }
 }
--- Session03-OOP/SundayShop/Giaolang.Shop/Program.cs
+++ Session03-OOP/SundayShop/Giaolang.Shop/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Giaolang.Shop
+﻿using Giaolang.Shop.Entities;
+
+namespace Giaolang.Shop
 //~ package com.microsoft.sqlserver.
 //đều có tên thương hiệu, chủ sở hữu sản phẩm
 //dấu chấm chia tầng thư mục quản lí cho gọn gàng
@@ -6,8 +8,61 @@
     internal class Program
     {
         static void Main(string[] args)
+        { 
+            CreateFruits();
+        }
+
+        public static void CreateFruits()
         {
-            Console.WriteLine("Hello, World!");
+            //tạo trái Mãng Cầu, y chang bên Java, import thay bằng using
+            //in thông tin qua ToString()
+            Fruit cau = new Fruit(); //object tạo ra, tốn ram, nhung vùng _ chứa giá trị default
+            Console.WriteLine("MÃNG CẦU details: " + cau.ToString());
+
+            Fruit sung = new Fruit("SS", "SUNG SƯỚNG", "SUNG LÀ TRÁI ĐẦU THỨ HAI TRONG MÂM NGŨ QUẢ", 5.0);//object tạo ra, tốn ram, nhung vùng _ chứa giá trị ĐC FILL QUA CONSTRUCTOR 
+
+            Console.WriteLine("SUNG details: " + sung.ToString());
+
+            //ko thấy định nghĩa document thì mình phải chuột vào property vào phần out chọn document file và click vào là xong
+            //trái dừa
+            Fruit dua = new Fruit("DD", "DỪA DỪA CX CX", "TRÁI DỪA LÀ...", 6.0);
+            Console.WriteLine("DUA details: " + dua.ToString());
+
+            //trái ĐU ĐỦ
+            //tham số hàm, phải đưa đúng số thứ stt khai báo biến, data type 
+            //Fruit du = new Fruit(7.0, "DU", "ĐU ĐỦ", "TRÁI DỪA LÀ..."); //ERROR LIỀN
+            Fruit du = new Fruit(price: 7.0, id: "DU", description: "ĐU ĐỦ", name: "TRÁI DỪA LÀ...");
+            //named-argument: truyền tham số cho hàm ko theo thứ tự đc yêu cầu
+            //nhưng chỉ rõ biến nào của hàm nhận value nào, là okie
+            //LỘN XỘN THỨ TỰ NHƯNG RÕ TÊN BIẾN THÌ OKIE
+
+            Console.WriteLine("DU details: " + du.ToString());
+
+            cau.SetId("MC");
+            cau.SetDesc("MÃNG CẦU LÀ ĐẦU CÂU CHUYỆN...");
+            cau.SetName("MÃNG CẦU");
+            cau.SetPrice(4.0);
+
+            Console.WriteLine("MÃNG CẦU details AFTER SETTING: " + cau.ToString());
+
+            /*
+            + Object có quyền thay đổi
+            + Mỗi vùng new là new 1 chỗ mới trong RAM để chứa biến
+            + Biến Object
+            + 2 vùng khác nhau
+            + Get, Set, Constructor
+            + new qua con đường truyền thống, new rỗng và sửa lại
+            + new qua cách truyền tham số full theo đúng thứ tự 
+            + new qua name argument
+            + DONE OOP: CĂN BẢN
+            + HỌC GET, SET KIỂU MỚI, KO LÀM THEO KIỂU CŨ NÀY NỮA !!!
+            + TƯƠNG ĐƯƠNG VÀ HAY HƠN JAVA GIẤU ĐI BIẾN _ ĐI VÀ HAY HƠN JAVA
+            + CÁI GET, SET NÀY TRONG NGHỀ GỌI LÀ BOILER PLACE đoạn code rất nhàm chắc
+            + Thật ra cần phải có ko có nó thì ko điều chỉnh đc thông tin, vấn đề là code này là rất nhàm chán, nó ko có hay
+            + Cho nên lý do thằng C# phải ở đầu class mới spam ra đc vì nó biết là cái này cùi!!!
+            + Java ko coi đó là điều tốn công
+            + C# sẽ che, ẩn cái phần Get, Set lại
+             */
         }
     }
 }
