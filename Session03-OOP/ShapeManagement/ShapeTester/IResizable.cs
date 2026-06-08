30309773bb3b9306c26e25474867fa16e1fc7b5e
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-06-08 12:10:16 +0700

    Week4b: OOP

---
+++ Session03-OOP/ShapeManagement/ShapeTester/IResizable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeTester
+{
+    /// <summary>
+    /// CLB của những gã co giãn...
+    /// </summary>
+    public interface IResizable
+    {
+        //interface là 1 dạng class Cha đặc biệt, chỉ chứa hàm ko có code - gọi là abstract method
+        //nó giống như CLB chiêu dụ có nội quy, hành động (abstract method)
+        //chờ Anh Em class Con tham gia dk member =>
+        //Anh Em hành động nội quy theo cách của họ => implementation, viết code cho hàm abtract ko code
+        //Xài: Khai Cha new Con...
+        //Interface hóa giải đa kế thừa, Con nhiều Cha
+        //Java C# giống nhau: Con chỉ có tối đa 1 Cha (kế thừa máu mủ họ hàng) và có thể tham gia nhiều CLB - interface
+
+        public void Resize(double size); //mỗi cạnh tăng lên mấy lần...
+
+        
+    }
+}
--- Session03-OOP/ShapeManagement/ShapeTester/Program.cs
+++ Session03-OOP/ShapeManagement/ShapeTester/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //PlayWithDisk();
+            //PlayWithRectangle();
+            //PlayWithTriangle();
+            PlayWithDiskReziable();
+        }
+
+        public static void PlayWithDiskReziable()
+        {
+            Disk d1 = new Disk(2, "RED");
+            d1.Resize(5);           
+            Console.WriteLine("Disk 1: " + d1.ToString()); //10, 314, 62.8
+           
+        }
+
+        public static void PlayWithTriangle()
+        {
+            //Shape r1 = new Rectangle(10, 20.0, "YELLOW");
+            Shape t1 = new Triangle(color: "YELLOW", a: 3, b: 4.0, c: 5.0);
+            Console.WriteLine("Triangle 1: " + t1.ToString()); //s: 6, p = 12
+
+            Triangle t2 = new Triangle()
+            {
+                //Color = "GREEN",
+                EdgeC = 10.0,
+                EdgeA = 6,
+                EdgeB = 8.0,
+            };
+
+            Console.WriteLine("Triangle 2: " + t2.ToString()); //s: 24, p = 24
+        }
+
+        public static void PlayWithDisk()
+        {
+            Shape d1 = new Disk(2, "RED");
+            Console.WriteLine("Disk 1: " + d1.ToString()); //s: 12,56, p = 12.56
+
+            Disk d2 = new Disk(3, "GREEN");
+            Console.WriteLine("Disk 2: " + d2.ToString()); //s: 28.26, p = 18.84
+                                                           //unit test, expected value
+                                                           //actual đoán xem chạy
+        }
+
+        public static void PlayWithRectangle()
+        {
+            //Shape r1 = new Rectangle(10, 20.0, "YELLOW");
+            Shape r1 = new Rectangle(color: "YELLOW", width: 10, height: 20.0);
+            Console.WriteLine("Rect 1: " + r1.ToString()); //s: 200, p = 60;
+
+            Rectangle r2 = new Rectangle()
+            {
+                //Color = "GREEN",
+                Height = 10.0,
+                Width = 10.0
+            };
+
+            Console.WriteLine("Rect 2: " + r2.ToString());
         }
     }
 }
