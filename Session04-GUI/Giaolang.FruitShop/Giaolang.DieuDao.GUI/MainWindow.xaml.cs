442e1cb192f151f300bac47cd448914890f93d3b
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-06-11 12:47:34 +0700

    Week05: GUI

---
+++ Session04-GUI/Giaolang.FruitShop/Giaolang.DieuDao.GUI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Giaolang.DieuDao.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //hứng nút đc bấm, thả vào biến thuộc class MessageBoxResult
+            MessageBoxResult answer =  MessageBox.Show("Are you sure?", "Quit?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (answer == MessageBoxResult.Yes) 
+            { 
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
