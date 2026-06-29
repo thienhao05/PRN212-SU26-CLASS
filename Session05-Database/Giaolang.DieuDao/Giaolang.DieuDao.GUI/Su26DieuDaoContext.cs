428befe822276d4992d7cb137f30363360b9de83
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-06-29 12:02:15 +0700

    w7: buoi 2

--- Session05-Database/Giaolang.DieuDao/Giaolang.DieuDao.GUI/Su26DieuDaoContext.cs
+++ Session05-Database/Giaolang.DieuDao/Giaolang.DieuDao.GUI/Su26DieuDaoContext.cs
@@ -22,7 +22,7 @@ public partial class Su26DieuDaoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345;database= SU26_DieuDao;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS22;uid=sa;pwd=12345;database= SU26_DieuDao;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
