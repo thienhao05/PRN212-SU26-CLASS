ffe6c21098e6ab4c96082810bcb54decb39e9be2
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-07-20 12:55:47 +0700

    feat: update week 9 b1

--- Session06-Delegates/NotificationApp/DelegateTesterLambdaExpression/Program.cs
+++ Session06-Delegates/NotificationApp/DelegateTesterLambdaExpression/Program.cs
@@ -13,14 +13,14 @@
             {
                 Console.WriteLine($"{x}^ 2 = {x * x}");
                 Console.WriteLine($"{x}^ 2 = {Math.Pow(x, 2)}");
-                
+
             };
 
             f1(10); //10^2 = 100
 
 
             //KĨ THUẬT RÚT GỌN CHỈ CÒN CÁI DÂY NỊT
-            MathFunc f2 =  (int x) =>
+            MathFunc f2 = (int x) =>
             {
                 Console.WriteLine($"{x}^ 2 = {x * x}");
                 Console.WriteLine($"{x}^ 2 = {Math.Pow(x, 2)}");
@@ -38,7 +38,7 @@
             };
 
             f3(10);
-             
+
             // Nếu chỉ có tham số và bỏ ngoặc
             MathFunc f4 = x =>
             {
