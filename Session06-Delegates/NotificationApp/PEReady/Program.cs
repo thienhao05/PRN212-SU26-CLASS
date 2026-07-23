be86d90d7928619f8441ca3c47e1fb0d522d4fde
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-07-23 15:00:00 +0700

    week 10: callback

---
+++ Session06-Delegates/NotificationApp/PEReady/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PEReady
+{
+    public delegate void BankNotiFunc(double amount);
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankNotiFunc t = amount => Console.WriteLine($"Quý khách vừa rút {amount} vnđ");
+
+            t(500); //màn hình sẽ in? Quý khách vừa rút 500000 vnđ
+        }
+    }
+}
+
+//HACK NÃO Ở NHÀ
+//MÌNH CÓ 1 Account Class để biểu diễn tài khoản ngân hàng của ai đó
+/*
+class Account
+{
+    _name;
+    _balance;
+    
+    hàm Widthdraw(amount)
+    {
+        if amount < balance 
+            _balance = _balance - _amount;
+        ngược lại ko cho rút
+        
+        //nếu rút thành công, thì in ra câu thông báo
+        "Quý khác vừa rút amount vnđ"
+        //CẤM XÀI cw trong hàm rút tiền
+        //NHỜ HÀM KHÁC IN GIÚP - DELEAGATE IN GIÚP
+    }
+
+}
+
+hàm truyền vào hàm
+
+ */
